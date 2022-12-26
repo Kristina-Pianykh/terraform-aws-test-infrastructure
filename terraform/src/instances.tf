5444921dd194272d872533c6318a7bbc7fddc0ed
Kristina Pianykh <kristinavrnrus@gmail.com>
2022-12-26 15:42:45 +0100

    chore: refactor aws_instacnes resource

--- terraform/src/instances.tf
+++ terraform/src/instances.tf
@@ -1,21 +1,21 @@
-locals {
-  instance-userdata = <<EOF
-#!/bin/bash
-# Use this for your user data (script from top to bottom)
-# install httpd (Linux 2 version)
-yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
-echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
-EOF
-}
+# locals {
+#   instance-userdata = <<EOF
+# #!/bin/bash
+# # Use this for your user data (script from top to bottom)
+# # install httpd (Linux 2 version)
+# yum update -y
+# yum install -y httpd
+# systemctl start httpd
+# systemctl enable httpd
+# echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+# EOF
+# }
 
 resource "aws_instance" "app_server" {
   ami                         = var.ami_id
-  instance_type               = "t2.micro"
+  instance_type               = var.instance_type
   count                       = var.instance_count
-  user_data_base64            = base64encode(local.instance-userdata)
+  user_data_base64            = base64encode(var.instance_userdata)
   vpc_security_group_ids      = [aws_security_group.launch_instance.id]
   associate_public_ip_address = true
 }
