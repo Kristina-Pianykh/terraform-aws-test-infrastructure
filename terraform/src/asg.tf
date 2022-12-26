b89761767fbe3d6b69b2cb367732be5996872c81
Kristina Pianykh <kristinavrnrus@gmail.com>
2022-12-26 20:05:51 +0100

    chore: add an asg policy

--- terraform/src/asg.tf
+++ terraform/src/asg.tf
@@ -11,11 +11,11 @@ resource "aws_launch_configuration" "demo-config" {
 }
 
 resource "aws_autoscaling_group" "demo-asg" {
-  name                 = "Autoscaling Group"
-  health_check_type    = "ELB"
-  vpc_zone_identifier  = [for subnet in aws_subnet.subnet : subnet.id]
-  desired_capacity     = 3
-  max_size             = 4
+  name                = "Autoscaling Group"
+  health_check_type   = "ELB"
+  vpc_zone_identifier = [for subnet in aws_subnet.subnet : subnet.id]
+  #   desired_capacity     = 3
+  max_size             = 3
   min_size             = 1
   force_delete         = true
   launch_configuration = aws_launch_configuration.demo-config.name
@@ -25,4 +25,19 @@ resource "aws_autoscaling_group" "demo-asg" {
 resource "aws_autoscaling_attachment" "test-attachment" {
   autoscaling_group_name = aws_autoscaling_group.demo-asg.id
   lb_target_group_arn    = aws_lb_target_group.test-target-group.arn
+}
+
+resource "aws_autoscaling_policy" "cpu-asg-policy" {
+  name                   = "CPUAsgPolicy"
+  adjustment_type        = "ChangeInCapacity"
+  autoscaling_group_name = aws_autoscaling_group.demo-asg.name
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 40.0
+  }
 }
