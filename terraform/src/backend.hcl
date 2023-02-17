954e56282e6297f7504cd916dbf101af8fab1578
Kristina Pianykh <kristinavrnrus@gmail.com>
2023-02-17 22:52:20 +0100

    chore: configure backend separately

---
+++ terraform/src/backend.hcl
@@ -0,0 +1,3 @@
+bucket         = "tf-demo-storage"
+region         = "eu-west-1"
+key            = "terraform-demo/terraform.tfstate"
--- terraform/src/main.tf
+++ terraform/src/main.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "tf-demo-storage"
-    key    = "terraform-demo/terraform.tfstate"
-    region = "eu-west-1"
   }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
