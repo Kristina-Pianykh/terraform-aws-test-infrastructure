ea8a18c2515af0e5297f0bfb701406c866289e06
Kristina Pianykh <kristinavrnrus@gmail.com>
2023-04-16 22:55:00 +0200

    fix: runnign shell script

--- terraform/src/rds.tf
+++ terraform/src/rds.tf
@@ -26,12 +26,8 @@ resource "null_resource" "db_setup" {
 
   provisioner "local-exec" {
     interpreter = [
-      "/bin/bash", "-c"
+      "/bin/bash"
     ]
-    command = <<EOT
-    echo '${path.module}'
-    pwd
-    ./import_data.sh -u ${var.db_username} -p ${var.db_password} -h ${split(":", aws_db_instance.demo_db.endpoint)[0]} -d ${aws_db_instance.demo_db.db_name} -f ${var.local_data_file_name} -t ${var.db_table_name} -P ${var.mysql_db_port}"""
-  EOT
+    command = "./import_data.sh -u ${var.db_username} -p ${var.db_password} -h ${split(":", aws_db_instance.demo_db.endpoint)[0]} -d ${aws_db_instance.demo_db.db_name} -f ${var.local_data_file_name} -t ${var.db_table_name} -P ${var.mysql_db_port}"
   }
 }
