dca9e59b72b3717c8ff1c7c8f6eaffdba3db06e9
Kristina Pianykh <kristinavrnrus@gmail.com>
2023-04-24 15:14:49 +0200

    feat: add a cluster and task definition

--- terraform/src/ecs.tf
+++ terraform/src/ecs.tf
@@ -1,5 +1,56 @@
-resource "aws_ecr_repository" "app_ecr_repo" {
+resource "aws_ecr_repository" "data_import_ecr" {
   name                 = "db_data_import"
   image_tag_mutability = "MUTABLE"
   force_delete         = true
+}
+
+resource "aws_ecs_cluster" "data_import" {
+  name = "data-import"
+}
+
+resource "aws_ecs_task_definition" "data_import" {
+  family                   = "data_import"
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "app-first-task",
+      "image": "${aws_ecr_repository.data_import_ecr.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 5000,
+          "hostPort": 5000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # use Fargate as the launch type
+  network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
+  memory                   = 512         # Specify the memory the container requires
+  cpu                      = 256         # Specify the CPU the container requires
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
